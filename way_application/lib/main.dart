7c329b08dab4a7a24f415ed6b10e9717d1e56c1b
WorldCreator892 <cor.vladik@yandex.ru>
2021-10-19 21:52:31 +0300

    first try

--- way_application/lib/main.dart
+++ way_application/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'page/LoginApp.dart';
 import 'page/Reg_Page.dart';
-void main() {
-  runApp(MaterialApp(
 
+void initFirebase() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
+
+
+void main() {
+  initFirebase();
+  runApp(MaterialApp(  
   initialRoute: '/',//перевод: начальное положение
   routes:{//страницы, которые есть в приложении
     //'/': (context) => MainScreen(),//основная страница
@@ -12,6 +20,7 @@ void main() {
     //'/RegPage':(context) => Reg_page(),
     '/': (context) => LoginApp(),
   },
+  
 ));
 }
 //LoginApp()
